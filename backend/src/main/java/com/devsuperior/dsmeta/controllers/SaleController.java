d7f5b2968c3b7080c6b5b9ce1d89f0bb337a9c7f
Celso Aquino <celso.aquino@ymail.com>
2022-09-07 15:35:58 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmeta.controllers;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sale")
+public class SaleController {
+
+    private final SaleService saleService;
+
+    public SaleController(SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    @GetMapping
+    public List<Sale> findSales() {
+        return saleService.findSales();
+    }
+ }
